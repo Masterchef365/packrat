cba362ad50820eb8a49850359ec39060c515ca31
Masterchef365 <duncan.freeman1@gmail.com>
2024-08-24 17:42:14 -0700

    Getting somewhere

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,6 +2,70 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use common::PackRatClient;
+use egui::Ui;
+use ewebsock_tarpc::WebSocketPoller;
+use poll_promise::Promise;
+use tarpc::client::NewClient;
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+pub struct App {
+    rx_text: Option<Promise<String>>,
+    client: PackRatClient,
+    data: AppData,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Default)]
+pub struct AppData {
+    data: u32,
+}
+
+impl App {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Load previous app state (if any).
+        let data: AppData = cc
+            .storage
+            .and_then(|storage| eframe::get_value(storage, eframe::APP_KEY))
+            .unwrap_or_default();
+
+        let client = PackRatClient::new(Default::default(), client_transport);
+
+        Self {
+            client: client.client,
+            rx_text: None,
+            data,
+        }
+    }
+}
+
+impl eframe::App for App {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, &self.data);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if let Some(promise) = &mut self.rx_text {
+                if let Some(value) = promise.ready() {
+                    ui.label(value);
+                } else {
+                    ui.label("Waiting for a response ...");
+                }
+            } else {
+                if ui.button("Do the thing").clicked() {
+                    let client = self.client.clone();
+                    self.rx_text = Some(Promise::spawn_local(async move {
+                        client
+                            .hello(tarpc::context::current(), "Hello from client".to_string())
+                            .await
+                            .unwrap()
+                    }));
+                }
+            }
+        });
+    }
+}
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -22,7 +86,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
     )
 }
 
@@ -39,50 +103,8 @@ fn main() {
             .start(
                 "the_canvas_id",
                 web_options,
-                Box::new(|cc| Ok(Box::new(TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(App::new(cc)))),
             )
             .await;
     });
-}
-
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-}
-
-impl Default for TemplateApp {
-    fn default() -> Self {
-        Self {}
-    }
-}
-
-impl TemplateApp {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let (client_transport, server_transport) = tarpc::transport::channel::unbounded();
-
-        // Load previous app state (if any).
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
-        let f = PackRatClient::new(Default::default(), client_transport);
-
-        Self {}
-    }
-}
-
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("hewwo");
-        });
-    }
 }
